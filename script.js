9c6b13d7655d165e6a9c623d4f5616022e13e116
odaysleft <vincentarasa254@gmail.com>
2025-04-24 10:39:57 +0300

    trying to figure out how to add the mole

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let mole = document.querySelector('.mole');
 let timeLeft = document.querySelector("#time-left");
 let score = document.querySelector("#score");
 let result = 0;
-
+let hitPosition;
 function randomSquare(){
      
      squares.forEach(
@@ -13,6 +13,32 @@ function randomSquare(){
      let randomSquare = squares[Math.floor(Math.random() * squares.length)];
 
 randomSquare.classList.add('mole')
+hitPosition = randomSquare.id;
 }
 
-//let timerId = setInterval(randomSquare, 20)
+
+let timerId; 
+function showMove(){
+     timerId = setInterval(randomSquare, 1000)
+}
+
+
+squares.forEach(
+     square => {
+          square.addEventListener('click', ()=> {
+               if(square.id == hitPosition){
+                    console.log(square.id)
+                    //showMove();
+                    result++;
+                    square.classList.add('mole')
+                    console.log(result)
+                    //clearInterval(timerId)
+               }
+               else{
+                    showMove()
+                    square[hitPosition].classList.add('mole')
+                    clearInterval(timerId)
+               }
+          })
+     }
+)
