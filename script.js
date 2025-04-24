ab6b1a068261fd2ed4a1f6c4c214c0f8e671681a
odaysleft <vincentarasa254@gmail.com>
2025-04-24 10:05:02 +0300

    declaring variable

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 let squares = document.querySelectorAll('.square');
+let mole = document.querySelector('.mole');
+let timeLeft = document.querySelector("#time-left");
+let score = document.querySelector("#score");
 
 squares.forEach(
      (square) => {
@@ -7,7 +10,14 @@ squares.forEach(
                () => {
 
                     
-                    square.classList.toggle("mole")
+                    //square.classList.toggle("mole")
+                    console.log(square.id)
+                    if(square.id == square.id){
+                         square.classList.add('mole')
+                    }
+                    else if(square.id != square.id){
+                         square.classList.remove('mole')
+                    }
                     
                }
           )
